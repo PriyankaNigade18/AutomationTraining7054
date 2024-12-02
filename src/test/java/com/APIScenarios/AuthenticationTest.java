be656f95c0b40ef4d09242c79fd624af3b4f0453
PriyankaN <piyu1818@gmail.com>
2024-12-02 22:26:37 +0530

    code updated

--- src/test/java/com/APIScenarios/AuthenticationTest.java
+++ src/test/java/com/APIScenarios/AuthenticationTest.java
@@ -1,5 +1,6 @@
 package com.APIScenarios;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.response.Response;
@@ -26,9 +27,11 @@ import java.util.List;
  * 
  */
 public class AuthenticationTest {
-  @Test
+  @Test(priority=1)
   public void testBasicAuthentication()
   {
+	  System.out.println("Test for Basic Authentication: Base64");
+
 	  Response res=given()
 			  		.auth().basic("postman","password")
 	  
@@ -39,4 +42,60 @@ public class AuthenticationTest {
 	  
 	  
   }
+  
+  
+  @Test(priority=2)
+  public void testDigestAuthentication()
+  {
+	  System.out.println("Test for Digest Authentication:SHA/MD5");
+	  Response res=given()
+	  .auth().digest("postman","password")
+	  
+	  .when().get("https://postman-echo.com/digest-auth");
+	  
+	  //log the response 
+	  res.then().log().body();
+	  
+  }
+  
+  
+  @Test(priority=3)
+  public void testBearerToken()
+  {	  System.out.println("Test for Bearer Token API");
+  
+  	Response res=given()
+  	.header("Authorization","Bearer 12345")
+  	
+  	.when().get("https://postman-echo.com");
+  
+  //log the response
+  
+
+  		res.then().log().body();
+	  
+  }
+  
+  
+  @Test
+  public void Oauth2Test()
+  {
+	  
+	  Response res=given()
+	  .auth().oauth2("Bearer 18182024")
+	  
+	  .when().get("https://postman-echo.com");
+	  
+	 int code=res.getStatusCode();
+	 Assert.assertEquals(code,200);
+	 System.out.println("Staus code is : "+code);
+
+	 res.then().log().body();
+	  
+	  
+	  
+	  
+	  
+	  
+  }
+  
 }
