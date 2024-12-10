53ea1acd928bbc23feb79ce0c7122f8b87c3bcda
PriyankaN <piyu1818@gmail.com>
2024-12-10 22:17:47 +0530

    AppiumAssignment

---
+++ src/test/java/com/TestMobileScenarios/ScreenshotTest.java
@@ -0,0 +1,17 @@
+package com.TestMobileScenarios;
+
+import org.testng.annotations.Test;
+
+public class ScreenshotTest {
+  @Test
+  public void f()
+  {
+//	  int i=1; //will declare globally
+//      File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//      File dest = new File(".//testdatafiles//screenshots//" + i + ".png");
+//      org.openqa.selenium.io.FileHandler.copy(src, dest);
+//      i++;
+//
+
+  }
+}
